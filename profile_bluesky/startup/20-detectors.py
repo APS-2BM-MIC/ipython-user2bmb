227d3060d8688a9c72b247233a8dc041a014cd9b
Francesco De Carlo <decarlof@gmail.com>
2018-01-16 16:36:46 -0600

    note that Edge PVs are different from Dimax

--- profile_bluesky/startup/20-detectors.py
+++ profile_bluesky/startup/20-detectors.py
@@ -1,10 +1,5 @@
 print(__file__)
 
-aps_current = EpicsSignalRO("S:SRcurrentAI", name="aps_current")
-
-## Beam Monitor Counts
-#bs_bm2 = EpicsSignalRO('BL14B:Det:BM2', name='bs_bm2')
-userCalc1_2bmb = EpicsSignalRO('2bmb:userCalc1', name='userCalc1_2bmb')
 scaler = EpicsScaler('2bmb:scaler1', name='scaler')
 
 try:
@@ -13,6 +8,7 @@ except TimeoutError:
     print("Could not connect to PCOIOC2:pco_dimax - is the IOC off?")
 
 try:
-    pco_edge = MyPcoDetector("PCOIOC3:", name="pco_edge")  # TODO: check PV prefix
+    # FIXME: edge detector has some different PVs than Dimax
+    pco_edge = MyPcoDetector("PCOIOC3:", name="pco_edge")
 except TimeoutError:
     print("Could not connect to PCOIOC3:pco_edge - is the IOC off?")
