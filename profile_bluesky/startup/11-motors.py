13b4fca4767bfe1a6b02bedf8f1c4960f6e3acd3
Francesco De Carlo <decarlof@gmail.com>
2018-01-16 16:21:45 -0600

    refactor

--- profile_bluesky/startup/11-motors.py
+++ profile_bluesky/startup/11-motors.py
@@ -3,12 +3,6 @@ print(__file__)
 
 # note: see 10-devices.py for how Devices are constructed
 
-A_shutter = AB_Shutter("2bma:A_shutter", name="A_shutter")
-B_shutter = AB_Shutter("2bma:B_shutter", name="B_shutter")
-A_filter = EpicsSignal("2bma:fltr1:select.VAL", name="A_filter")
-A_mirror1 = Mirror1_A("2bma:M1", name="A_mirror1")
-A_slit1_h_center = EpicsSignal("2bma:Slit1Hcenter", name="A_slit1_h_center")
-
 tomo_shutter = Motor_Shutter("2bma:m23", name="tomo_shutter")
 
 # generic motor name: {station}m{number}
@@ -23,8 +17,8 @@ am31 = EpicsMotor("2bma:m31", name="am31")    # ? DSArm
 am32 = EpicsMotor("2bma:m32", name="am32")    # ? M2Y
 
 # report these in default `wa` command
-BlueskyMagics.positioners += [am7, am25, am26, am27, am28, am29, am30, am31, am32]
-BlueskyMagics.positioners += [tomo_shutter.motor]
+append_wa_motor_list(am7, am25, am26, am27, am28, am29, am30, am31, am32)
+append_wa_motor_list(tomo_shutter.motor)
 
 
 # TODO: should some of these be part of a Device? sample&rot&pos stages are grouped, for example
@@ -38,29 +32,5 @@ furnaceY = EpicsMotor("2bma:m55", name="furnaceY")
 bm4      = EpicsMotor("2bmb:m4",  name="bm4")               # posStage in B LAT
 bm57     = EpicsMotor("2bmb:m57", name="bm57")              # posStage in B SAT
 
-BlueskyMagics.positioners += [am20, am46, am49, bm82, bm63, bm100]
-BlueskyMagics.positioners += [furnaceY, bm4, bm57]
-
-
-# TODO: these are NOT motors!
-
-pso1     = PSO_Device("2bmb:PSOFly1:", name="pso1")
-pso2     = PSO_Device("2bmb:PSOFly2:", name="pso2")
-tableFly2_sseq_PROC = EpicsSignal(
-          "2bmb:tableFly2:sseq2.PROC", name="tableFly2_sseq_PROC")
-
-# TODO: assign more meaningful names?
-caputRecorder1 = EpicsSignal("2bmb:caputRecorderGbl_1", name="caputRecorder1", string=True)     # prefix
-caputRecorder2 = EpicsSignal("2bmb:caputRecorderGbl_2", name="caputRecorder2", string=True)     # prefix #
-caputRecorder3 = EpicsSignal("2bmb:caputRecorderGbl_3", name="caputRecorder3", string=True)     # auto-increase #
-caputRecorder4 = EpicsSignal("2bmb:caputRecorderGbl_4", name="caputRecorder4", string=True)     # sample name
-caputRecorder5 = EpicsSignal("2bmb:caputRecorderGbl_5", name="caputRecorder5", string=True)     # lens mag
-caputRecorder6 = EpicsSignal("2bmb:caputRecorderGbl_6", name="caputRecorder6", string=True)     # sam-det dist(mm)
-caputRecorder7 = EpicsSignal("2bmb:caputRecorderGbl_7", name="caputRecorder7", string=True)     # scinThickness(um)
-caputRecorder8 = EpicsSignal("2bmb:caputRecorderGbl_8", name="caputRecorder8", string=True)     # scinType
-caputRecorder9 = EpicsSignal("2bmb:caputRecorderGbl_9", name="caputRecorder9", string=True)     # filter
-caputRecorder10 = EpicsSignal("2bmb:caputRecorderGbl_10", name="caputRecorder10", string=True)  # proj #
-caputRecorder_filepath = EpicsSignal("2bmb:caputRecorderGbl_filepath", name="caputRecorder_filepath", string=True)
-caputRecorder_filename = EpicsSignal("2bmb:caputRecorderGbl_filename", name="caputRecorder_filename", string=True)
-
-userCalcs_2bmb = userCalcsDevice("2bmb:", name="userCalcs_2bmb")
+append_wa_motor_list(am20, am46, am49, bm82, bm63, bm100)
+append_wa_motor_list(furnaceY, bm4, bm57)
---
+++ profile_bluesky/startup/20-signals.py
@@ -0,0 +1,48 @@
+print(__file__)
+
+
+# note: see 10-devices.py for how Devices are constructed
+
+A_shutter = AB_Shutter("2bma:A_shutter", name="A_shutter")
+B_shutter = AB_Shutter("2bma:B_shutter", name="B_shutter")
+A_filter = EpicsSignal("2bma:fltr1:select.VAL", name="A_filter")
+A_mirror1 = Mirror1_A("2bma:M1", name="A_mirror1")
+A_slit1_h_center = EpicsSignal("2bma:Slit1Hcenter", name="A_slit1_h_center")
+
+
+pso1     = PSO_Device("2bmb:PSOFly1:", name="pso1")
+pso2     = PSO_Device("2bmb:PSOFly2:", name="pso2")
+tableFly2_sseq_PROC = EpicsSignal(
+          "2bmb:tableFly2:sseq2.PROC", name="tableFly2_sseq_PROC")
+
+# TODO: assign more meaningful names?
+"""
+=== ======================
+#   name
+=== ======================
+1   prefix
+2   prefix #
+3   auto-increase #
+4   sample name
+5   lens mag
+6   sam-det dist (mm)
+7   scinThickness (um)
+8   scinType
+9   filter
+10  proj #
+=== ======================
+"""
+caputRecorder1 = EpicsSignal("2bmb:caputRecorderGbl_1", name="caputRecorder1", string=True)     # prefix
+caputRecorder2 = EpicsSignal("2bmb:caputRecorderGbl_2", name="caputRecorder2", string=True)     # prefix #
+caputRecorder3 = EpicsSignal("2bmb:caputRecorderGbl_3", name="caputRecorder3", string=True)     # auto-increase #
+caputRecorder4 = EpicsSignal("2bmb:caputRecorderGbl_4", name="caputRecorder4", string=True)     # sample name
+caputRecorder5 = EpicsSignal("2bmb:caputRecorderGbl_5", name="caputRecorder5", string=True)     # lens mag
+caputRecorder6 = EpicsSignal("2bmb:caputRecorderGbl_6", name="caputRecorder6", string=True)     # sam-det dist(mm)
+caputRecorder7 = EpicsSignal("2bmb:caputRecorderGbl_7", name="caputRecorder7", string=True)     # scinThickness(um)
+caputRecorder8 = EpicsSignal("2bmb:caputRecorderGbl_8", name="caputRecorder8", string=True)     # scinType
+caputRecorder9 = EpicsSignal("2bmb:caputRecorderGbl_9", name="caputRecorder9", string=True)     # filter
+caputRecorder10 = EpicsSignal("2bmb:caputRecorderGbl_10", name="caputRecorder10", string=True)  # proj #
+caputRecorder_filepath = EpicsSignal("2bmb:caputRecorderGbl_filepath", name="caputRecorder_filepath", string=True)
+caputRecorder_filename = EpicsSignal("2bmb:caputRecorderGbl_filename", name="caputRecorder_filename", string=True)
+
+userCalcs_2bmb = userCalcsDevice("2bmb:", name="userCalcs_2bmb")
