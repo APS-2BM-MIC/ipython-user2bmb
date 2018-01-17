4727afa1616fc879cf990cfafc69496416b6a5ee
Francesco De Carlo <decarlof@gmail.com>
2018-01-16 18:32:48 -0600

    aliases; more signals for EdgeMultiPosScan()

--- profile_bluesky/startup/20-signals.py
+++ profile_bluesky/startup/20-signals.py
@@ -22,7 +22,18 @@ pso2     = PSO_Device("2bmb:PSOFly2:", name="pso2")
 tableFly2_sseq_PROC = EpicsSignal(
           "2bmb:tableFly2:sseq2.PROC", name="tableFly2_sseq_PROC")
 
-# TODO: assign more meaningful names?
+caputRecorder1 = EpicsSignal("2bmb:caputRecorderGbl_1", name="caputRecorder1", string=True)     # prefix
+caputRecorder2 = EpicsSignal("2bmb:caputRecorderGbl_2", name="caputRecorder2", string=True)     # prefix #
+caputRecorder3 = EpicsSignal("2bmb:caputRecorderGbl_3", name="caputRecorder3", string=True)     # auto-increase #
+caputRecorder4 = EpicsSignal("2bmb:caputRecorderGbl_4", name="caputRecorder4", string=True)     # sample name
+caputRecorder5 = EpicsSignal("2bmb:caputRecorderGbl_5", name="caputRecorder5", string=True)     # lens mag
+caputRecorder6 = EpicsSignal("2bmb:caputRecorderGbl_6", name="caputRecorder6", string=True)     # sam-det dist(mm)
+caputRecorder7 = EpicsSignal("2bmb:caputRecorderGbl_7", name="caputRecorder7", string=True)     # scinThickness(um)
+caputRecorder8 = EpicsSignal("2bmb:caputRecorderGbl_8", name="caputRecorder8", string=True)     # scinType
+caputRecorder9 = EpicsSignal("2bmb:caputRecorderGbl_9", name="caputRecorder9", string=True)     # filter
+caputRecorder10 = EpicsSignal("2bmb:caputRecorderGbl_10", name="caputRecorder10", string=True)  # proj #
+caputRecorder_filepath = EpicsSignal("2bmb:caputRecorderGbl_filepath", name="caputRecorder_filepath", string=True)
+caputRecorder_filename = EpicsSignal("2bmb:caputRecorderGbl_filename", name="caputRecorder_filename", string=True)
 """
 === ======================
 #   name
@@ -39,18 +50,21 @@ tableFly2_sseq_PROC = EpicsSignal(
 10  proj #
 === ======================
 """
-caputRecorder1 = EpicsSignal("2bmb:caputRecorderGbl_1", name="caputRecorder1", string=True)     # prefix
-caputRecorder2 = EpicsSignal("2bmb:caputRecorderGbl_2", name="caputRecorder2", string=True)     # prefix #
-caputRecorder3 = EpicsSignal("2bmb:caputRecorderGbl_3", name="caputRecorder3", string=True)     # auto-increase #
-caputRecorder4 = EpicsSignal("2bmb:caputRecorderGbl_4", name="caputRecorder4", string=True)     # sample name
-caputRecorder5 = EpicsSignal("2bmb:caputRecorderGbl_5", name="caputRecorder5", string=True)     # lens mag
-caputRecorder6 = EpicsSignal("2bmb:caputRecorderGbl_6", name="caputRecorder6", string=True)     # sam-det dist(mm)
-caputRecorder7 = EpicsSignal("2bmb:caputRecorderGbl_7", name="caputRecorder7", string=True)     # scinThickness(um)
-caputRecorder8 = EpicsSignal("2bmb:caputRecorderGbl_8", name="caputRecorder8", string=True)     # scinType
-caputRecorder9 = EpicsSignal("2bmb:caputRecorderGbl_9", name="caputRecorder9", string=True)     # filter
-caputRecorder10 = EpicsSignal("2bmb:caputRecorderGbl_10", name="caputRecorder10", string=True)  # proj #
-caputRecorder_filepath = EpicsSignal("2bmb:caputRecorderGbl_filepath", name="caputRecorder_filepath", string=True)
-caputRecorder_filename = EpicsSignal("2bmb:caputRecorderGbl_filename", name="caputRecorder_filename", string=True)
+# assign more meaningful names
+gbl_prefix = caputRecorder1
+gbl_prefix_num = caputRecorder2
+gbl_auto_increase = caputRecorder3
+gbl_sample_name = caputRecorder4
+gbl_lens_mag = caputRecorder5
+gbl_sam_det_dist = caputRecorder6
+gbl_scin_thickness = caputRecorder7
+gbl_scin_type = caputRecorder8
+gbl_filter = caputRecorder9
+gbl_proj_num = caputRecorder10
+
+arg9  = EpicsSignal("2bmb:caputRecorderArg9Value.VAL",  name="arg9")
+arg13 = EpicsSignal("2bmb:caputRecorderArg13Value.VAL", name="arg13")
+arg14 = EpicsSignal("2bmb:caputRecorderArg14Value.VAL", name="arg14")
 
 interlaceFlySub_2bmb = SynApps_Record_asub("2bmb:iFly:interlaceFlySub", name="interlaceFlySub_2bmb")
 savedata_2bmb = SynApps_saveData_Device("2bmb:saveData", name="savedata_2bmb")
@@ -58,3 +72,5 @@ savedata_2bmb = SynApps_saveData_Device("2bmb:saveData", name="savedata_2bmb")
 preTemp = EpicsSignal("2bmb:ET2k:1:Temperature.VAL", name="preTemp")
 
 userCalcs_2bmb = userCalcsDevice("2bmb:", name="userCalcs_2bmb")
+s1_d1dmm_calc = EpicsSignal("2bmS1:D1Dmm_calc", name="s1_d1dmm_calc")
+s1_d1done_read = EpicsSignal("2bmS1:D1done_read", name="s1_d1done_read")
