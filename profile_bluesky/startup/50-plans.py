c5585f0b3446237c7c43099d5972b32dbe63ab29
jemian <prjemian@gmail.com>
2018-02-01 18:06:08 -0600

    #2 discussion notes from today

--- profile_bluesky/startup/50-plans.py
+++ profile_bluesky/startup/50-plans.py
@@ -1,3 +1,73 @@
 print(__file__)
 
 # Bluesky plans (scans)
+
+
+# FIXME:  These are just notes from a phone conversation 2018-02-01 with Dan Allan
+
+
+def bluesky_plan_circa_2015():
+    """This syntax was refactored into `abs_set()`"""
+    yield Msg(‘set’, pso1, ‘Taxi’)
+
+
+
+
+# The plan yields a message.
+# These are all the same.
+
+
+def example_plan1():
+    yield from abs_set(pso1, ‘Taxi’, group=’A’)
+    yield from wait(‘A’)
+    yield from abs_set(pso1, ‘Fly’, group=’B’)
+    yield from wait(‘B’)
+
+
+def example_plan2():
+    yield from abs_set(pso1, ‘Taxi’, wait=True)
+    yield from abs_set(pso1, ‘Fly’, wait=True)
+
+
+def example_plan3():
+    yield from mv(pso1, ‘Taxi’)  # waits by default
+    yield from mv(pso1, ‘Fly’)
+
+"""
+The idea with mv is this:
+`mv(theta, 0, phi, 1, alpha, 3)`  # in parallel -- wait until all are done
+
+Note:
+
+===========================   ======================================
+don't use in plan()           correct way to write in plan()
+===========================   ======================================
+some.device.put("config")     yield from mv(some.device, "config")
+motor.move(52)                yield from mv(motor, 52)
+motor.velocity.put(5)         yield from mv(motor.velocity, 5)
+===========================   ======================================
+
+By using `yield from`, BlueSky is able to handle and recover from 
+interruptions such as the user pressing `^C`.  Also, the RE can 
+differentiate if the plan is being run in simulation and not actually
+change the underlying EPICS PV.
+
+
+# RunEngine iterates through plan, receives Msg(‘set’, pso1, ‘Taxi’)
+
+
+# RunEngine looks at RunEngine._command_registry and finds that ‘set’ is mapped to RunEngine._set. It calls RunEngine._set(Msg(‘set’, pso1, ‘Taxi’)).
+# RunEngine._set calls pso1.set(‘taxi’) and gets back a status object.
+# The RE stashes that status object in a local cache. Perhaps later the plan will ask the RunEngine to wait on the status object via Msg(‘wait’, …)
+
+
+Additionally, RunEngine._set is doing other good stuff for us, like keeping track of the fact that this device is in motion.
+
+
+
+
+Note: Remember to search all plans for `time.sleep(X)` and replace with `yield from sleep(X)`
+
+
+All these “plan stubs” (plans that don’t generate runs like count or scan do) are in bluesky.plans pre-0.11.0 and bluesky.plan_stubs in v0.11.0+.
+"""
