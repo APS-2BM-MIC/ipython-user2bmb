ad452cde0f2816d32aa92b66b01e2bd22cd396db
Francesco De Carlo <decarlof@gmail.com>
2018-05-09 15:01:05 -0500

    PVA plugin adapter

--- profile_pg3_only/startup/25-PG3-grasshopper.py
+++ profile_pg3_only/startup/25-PG3-grasshopper.py
@@ -4,6 +4,15 @@ print(__file__)
 
 from ophyd import PointGreyDetectorCam
 
+# PVA plugin not staged enabled/disabled yet, be prepared
+class MyPvaPlugin(PluginBase):
+    """PVA plugin (not in ophyd)"""
+    _default_suffix = 'EDGEDSC:'
+    _suffix_re = 'pva\d:'
+    _html_docs = ['unconfigured.html']
+    _plugin_type = 'NDPluginPva'
+    pva_image_pv_name = ADComponent(EpicsSignalRO, "PvName_RBV")
+
 
 class MyPointGreyDetector(SingleTrigger, AreaDetector):
     """PointGrey Grasshopper3 detector as used by 2-BM-B tomography"""
@@ -11,6 +20,7 @@ class MyPointGreyDetector(SingleTrigger, AreaDetector):
     cam = ADComponent(PointGreyDetectorCam, "cam1:")
     image = ADComponent(ImagePlugin, "image1:")
     hdf1 = ADComponent(HDF5Plugin, "HDF1:")
+    pva1 = ADComponent(MyPvaPlugin, "Pva1:")
 
 
 pg3_det = MyPointGreyDetector(
