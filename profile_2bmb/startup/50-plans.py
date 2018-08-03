88a807cb25c9210855907ae679943de87e498533
Pete Jemian <prjemian@gmail.com>
2018-08-03 16:32:14 -0500

    #27 works right when wait for HDF5 to catch up

--- profile_2bmb/startup/50-plans.py
+++ profile_2bmb/startup/50-plans.py
@@ -5,9 +5,11 @@ print(__file__)
 
 def basic(*args, darks=1, whites=1, images=1):
     det = pg3_det
-    luftpause = 1
-    
-    yield from bps.mv(det.hdf1.num_capture, darks+whites+images)
+        
+    yield from bps.mv(
+        det.hdf1.num_capture, darks+whites+images,
+        det.hdf1.compression, "zlib",
+    )
     yield from bps.stage(det)
 
     print("%d dark frames" % darks)
@@ -15,28 +17,36 @@ def basic(*args, darks=1, whites=1, images=1):
         det.cam.frame_type, 1,
         det.cam.num_images, darks,
     )
-    yield from bps.sleep(luftpause)
     yield from bps.trigger(det)
+    while det.hdf1.num_captured.value < darks:
+        yield from bps.sleep(0.01)
 
     print("%d white frames" % whites)
     yield from bps.mv(
         det.cam.frame_type, 2,
         det.cam.num_images, whites,
     )
-    yield from bps.sleep(luftpause)
     yield from bps.trigger(det)
+    while det.hdf1.num_captured.value < darks+whites:
+        yield from bps.sleep(0.01)
 
     print("%d image frames" % images)
     yield from bps.mv(
         det.cam.frame_type, 0,
         det.cam.num_images, images,
     )
-    yield from bps.sleep(luftpause)
     yield from bps.trigger(det)
+    while det.hdf1.num_captured.value < darks+whites+images:
+        yield from bps.sleep(0.01)
 
     print("waiting for HDF5 writer to finish")
     # instead of setting det.hdf1.blocking_callbacks to "Yes"
     while det.hdf1.capture.value != 0:
         yield from bps.sleep(0.01)
+    yield from bps.mv(
+        det.hdf1.num_capture, 1,
+        det.cam.num_images, 1,
+        det.hdf1.compression, "None",
+    )
 
     yield from bps.unstage(det)
