91468395ef75a114de9af5faba3010f2a69ce47d
Pete Jemian <prjemian@gmail.com>
2018-08-03 16:43:13 -0500

    #27 working demo or algorithm

--- profile_2bmb/startup/50-plans.py
+++ profile_2bmb/startup/50-plans.py
@@ -3,9 +3,19 @@ print(__file__)
 """plans"""
 
 
-def basic(*args, darks=1, whites=1, images=1):
+def demo_of_issue_27(*args, darks=1, whites=1, images=1):
     det = pg3_det
-        
+
+    def shoot(fr_type, num=1, expected=None):
+        expected = expected or num
+        yield from bps.mv(
+            det.cam.frame_type, fr_type,
+            det.cam.num_images, num,
+        )
+        yield from bps.trigger(det)
+        while det.hdf1.num_captured.value < expected:
+            yield from bps.sleep(0.01)
+   
     yield from bps.mv(
         det.hdf1.num_capture, darks+whites+images,
         det.hdf1.compression, "zlib",
@@ -13,36 +23,19 @@ def basic(*args, darks=1, whites=1, images=1):
     yield from bps.stage(det)
 
     print("%d dark frames" % darks)
-    yield from bps.mv(
-        det.cam.frame_type, 1,
-        det.cam.num_images, darks,
-    )
-    yield from bps.trigger(det)
-    while det.hdf1.num_captured.value < darks:
-        yield from bps.sleep(0.01)
+    yield from shoot(1, darks, darks)
 
     print("%d white frames" % whites)
-    yield from bps.mv(
-        det.cam.frame_type, 2,
-        det.cam.num_images, whites,
-    )
-    yield from bps.trigger(det)
-    while det.hdf1.num_captured.value < darks+whites:
-        yield from bps.sleep(0.01)
+    yield from shoot(2, whites, darks+whites)
 
     print("%d image frames" % images)
-    yield from bps.mv(
-        det.cam.frame_type, 0,
-        det.cam.num_images, images,
-    )
-    yield from bps.trigger(det)
-    while det.hdf1.num_captured.value < darks+whites+images:
-        yield from bps.sleep(0.01)
+    yield from shoot(0, images, darks+whites+images)
 
     print("waiting for HDF5 writer to finish")
     # instead of setting det.hdf1.blocking_callbacks to "Yes"
     while det.hdf1.capture.value != 0:
         yield from bps.sleep(0.01)
+
     yield from bps.mv(
         det.hdf1.num_capture, 1,
         det.cam.num_images, 1,
