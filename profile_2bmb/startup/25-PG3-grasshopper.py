f5b103b23d0abdae30127ed31b1d68faa7a06741
Pete Jemian <prjemian@gmail.com>
2018-08-19 14:43:03 -0500

    fixes #43

--- profile_2bmb/startup/25-PG3-grasshopper.py
+++ profile_2bmb/startup/25-PG3-grasshopper.py
@@ -102,71 +102,12 @@ example setup for the PVA plugin (from FileStoreTIFFSquashing)
 
 """
 
-# APS_devices.AD_FrameType_schemes
-# APS_devices.AD_setup_FrameType
-# TODO: switch to the APS_devices code after 2018-08-04
 
-AD_FrameType_schemes = {
-    "reset" : dict(             # default names from Area Detector code
-        ZRST = "Normal",
-        ONST = "Background",
-        TWST = "FlatField",
-        ),
-    "NeXus" : dict(             # NeXus (typical locations)
-        ZRST = "/entry/data/data",
-        ONST = "/entry/data/dark",
-        TWST = "/entry/data/white",
-        ),
-    "DataExchange" : dict(      # APS Data Exchange
-        ZRST = "/exchange/data",
-        ONST = "/exchange/data_dark",
-        TWST = "/exchange/data_white",
-        ),
-}
-
-
-
-def AD_setup_FrameType(prefix, scheme="NeXus"):
-    """
-    configure so frames are identified & handled by type (dark, white, or image)
-    
-    PARAMETERS
-        prefix (str) : EPICS PV prefix of area detector, such as "13SIM1:"
-        scheme (str) : any key in the `AD_FrameType_schemes` dictionary
-    
-    This routine prepares the EPICS Area Detector to identify frames
-    by image type for handling by clients, such as the HDF5 file writing plugin.
-    With the HDF5 plugin, the `FrameType` PV is added to the NDattributes
-    and then used in the layout file to direct the acquired frame to
-    the chosen dataset.  The `FrameType` PV value provides the HDF5 address
-    to be used.
-    
-    To use a different scheme than the defaults, add a new key to
-    the `AD_FrameType_schemes` dictionary, defining storage values for the
-    fields of the EPICS `mbbo` record that you will be using.
-    
-    see: https://github.com/BCDA-APS/use_bluesky/blob/master/notebooks/images_darks_flats.ipynb
-    
-    EXAMPLE::
-    
-        AD_setup_FrameType("2bmbPG3:", scheme="DataExchange")
-    
-    * Call this function *before* creating the ophyd area detector object
-    * use lower-level PyEpics interface
-    """
-    db = AD_FrameType_schemes.get(scheme)
-    if db is None:
-        msg = "unknown AD_FrameType_schemes scheme: {}".format(scheme)
-        msg += "\n Should be one of: " + ", ".join(AD_FrameType_schemes.keys())
-        raise ValueError(msg)
-
-    template = "{}cam1:FrameType{}.{}"
-    for field, value in db.items():
-        epics.caput(template.format(prefix, "", field), value)
-        epics.caput(template.format(prefix, "_RBV", field), value)
-
-
-# AD_setup_FrameType(EPICS_PV_prefix["PG3 PointGrey Grasshopper3"], scheme="DataExchange")
+if False:   # run this code only as-needed
+    APS_devices.AD_setup_FrameType(
+        EPICS_PV_prefix["PG3 PointGrey Grasshopper3"], 
+        scheme="DataExchange"
+    )
 
 
 pg3_det = MyPointGreyDetector(
