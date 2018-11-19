042caed717396c1675b31945050d9e19e604dc6a
Francesco De Carlo <decarlof@gmail.com>
2018-11-19 17:06:27 -0600

    #54 out with the old

--- profile_2bmb/startup/ioc2bmbmona/run_mona_ioc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# file: run_mona_ioc.sh
-# purpose: provide PVs for MONA modules to advise BlueSky
-
-#softIoc -d ./mona.db
-
-# run in screen session: 
-/usr/bin/screen -dm -S mona -h 5000 softIoc -d ./mona.db
