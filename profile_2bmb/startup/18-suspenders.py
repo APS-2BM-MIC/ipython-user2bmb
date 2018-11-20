750246be0cb63ad3c12199006787e2be7f6d737c
Francesco De Carlo <decarlof@gmail.com>
2018-11-20 11:19:17 -0600

    #53 allow choice to latch this suspender
    (default is to latch and not resume automatically)

--- profile_2bmb/startup/18-suspenders.py
+++ profile_2bmb/startup/18-suspenders.py
@@ -5,20 +5,32 @@ print(__file__)
 from bluesky.suspenders import SuspenderBase
 
 
-class SuspendAndLatchWhenChanged(SuspenderBase):
+class SuspendWhenChanged(SuspenderBase):
     """
     Bluesky suspender
     
-    Do not resume when the monitored value deviates from the expected.
+    Suspend when the monitored value deviates from the expected.
+    Only resume if allowed AND when monitored equals expected.
+    
+    USAGE::
+
+        # pause if this value changes in our session
+        # note: this suspender is designed to require Bluesky restart if value changes
+        suspend_instrument_in_use = SuspendWhenChanged(instrument_in_use)
+        RE.install_suspender(suspend_instrument_in_use)
+    
+    Note: This should be moved into APS_Bluesky_tools.
     """
     # see: http://nsls-ii.github.io/bluesky/_modules/bluesky/suspenders.html#SuspendCeil
     
     def __init__(self, signal, *, 
-                expected_value=None, 
+                expected_value=None,
+                allow_resume=False,
                 sleep=0, pre_plan=None, post_plan=None, tripped_message='',
                 **kwargs):
         
         self.expected_value = expected_value or signal.value
+        self.allow_resume = allow_resume
         super().__init__(signal, 
             sleep=sleep, 
             pre_plan=pre_plan, 
@@ -30,8 +42,7 @@ class SuspendAndLatchWhenChanged(SuspenderBase):
         return value != self.expected_value
 
     def _should_resume(self, value):
-        # return value == self.expected_value
-        return False    # latching
+        return self.allow_resume and value == self.expected_value
 
     def _get_justification(self):
         if not self.tripped:
@@ -41,5 +52,7 @@ class SuspendAndLatchWhenChanged(SuspenderBase):
             self._sig.name,
             self._sig.get(),
             self.expected_value)
+        if not self.allow_resume:
+            just += '.  "RE.abort" and then restart session to use new configuration.'
         return ': '.join(s for s in (just, self._tripped_message)
                          if s)
