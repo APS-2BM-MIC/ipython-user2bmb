f45704b91fc4bb29f2bffd66803465bba4c5570d
Francesco De Carlo <decarlof@gmail.com>
2018-11-20 11:19:57 -0600

    fixes #53 configure for operations or test mode

--- profile_2bmb/startup/20-signals.py
+++ profile_2bmb/startup/20-signals.py
@@ -20,50 +20,52 @@ class ExperimentInfo(Device):
     user_email = Component(EpicsSignalRO, "UserEmail", string=True)
 
 
-try:
-    A_shutter = APS_devices.ApsPssShutterWithStatus(
-        "2bma:A_shutter", 
-        "PA:02BM:STA_A_FES_OPEN_PL", 
-        name="A_shutter")
-    B_shutter = APS_devices.ApsPssShutterWithStatus(
-        "2bma:B_shutter", 
-        "PA:02BM:STA_B_SBS_OPEN_PL", 
-        name="B_shutter")
-except Exception as _exc:
-    print(_exc)
-
-
-mona = MonaModuleSignals(name="mona")
 instrument_in_use = EpicsSignalRO(
     "2bm:instrument_in_use", 
     name="instrument_in_use")
 
-# pause if this value changes in our session
-# note: this suspender is designed by us to NEVER RESUME
-suspend_instrument_in_use = SuspendAndLatchWhenChanged(instrument_in_use)
-RE.install_suspender(suspend_instrument_in_use)
-
-if False:
-    # pete's testing items (not EPICS, just ophyd)
-    import ophyd.sim
-    m1 = ophyd.sim.motor
-    sig = ophyd.sim.noisy_det   # Gaussian(m1), sigma=1, x0=0, 10% noise, max=1
-    #  RE(bp.scan([sig], m1, -3, 3, 12))
-
-
-def using_beam_in_2bmb():
+def operations_in_2bmb():
+    """returns True if allowed to use X-ray beam in 2-BM-B station"""
     v = instrument_in_use.value
     enums = instrument_in_use.enum_strs
     return enums[v] == "2-BM-B"
 
+# pause if this value changes in our session
+# note: this suspender is designed to require Bluesky restart if value changes
+suspend_instrument_in_use = SuspendWhenChanged(instrument_in_use)
+RE.install_suspender(suspend_instrument_in_use)
+
+
+mona = MonaModuleSignals(name="mona")
+
 
 aps = APS_devices.ApsMachineParametersDevice(name="APS")
 sd.baseline.append(aps)
-
 aps_current = aps.current
-#sd.monitors.append(aps_current)
 
-if aps.inUserOperations:        # and using_beam_in_2bmb():
+
+user_info = ExperimentInfo("2bmS1:", name="user_info")
+sd.baseline.append(user_info)
+
+
+if operations_in_2bmb() and aps.inUserOperations:
+    # setup for operations using the X-ray beam
+    
+    # record storage ring current as additional data stream
+    sd.monitors.append(aps.current)
+
+    try:
+        A_shutter = APS_devices.ApsPssShutterWithStatus(
+            "2bma:A_shutter", 
+            "PA:02BM:STA_A_FES_OPEN_PL", 
+            name="A_shutter")
+        B_shutter = APS_devices.ApsPssShutterWithStatus(
+            "2bma:B_shutter", 
+            "PA:02BM:STA_B_SBS_OPEN_PL", 
+            name="B_shutter")
+    except Exception as _exc:
+        print(_exc)
+
     # no scans until A_shutter is open
     suspend_A_shutter = bluesky.suspenders.SuspendFloor(A_shutter.pss_state, 1)
     #suspend_A_shutter.install(RE)
@@ -72,9 +74,19 @@ if aps.inUserOperations:        # and using_beam_in_2bmb():
     # no scans if aps.current is too low
     suspend_APS_current = bluesky.suspenders.SuspendFloor(aps_current, 2, resume_thresh=10)
     RE.install_suspender(suspend_APS_current)
-    pass
+
+else:
+    # setup for test mode without the beam
+    
+    # define the simulated shutter objects here
+    A_shutter = APS_devices.SimulatedApsPssShutterWithStatus(name="A_shutter")
+    B_shutter = APS_devices.SimulatedApsPssShutterWithStatus(name="B_shutter")
+
+    # pete's testing items (not EPICS, just ophyd)
+    import ophyd.sim
+    m1 = ophyd.sim.motor
+    sig = ophyd.sim.noisy_det   # Gaussian(m1), sigma=1, x0=0, 10% noise, max=1
+    #  RE(bp.scan([sig], m1, -3, 3, 12))
+
 
 #pf4 = APS_devices.DualPf4FilterBox("2bmb:pf4:", name="pf4")
-
-user_info = ExperimentInfo("2bmS1:", name="user_info")
-sd.baseline.append(user_info)
